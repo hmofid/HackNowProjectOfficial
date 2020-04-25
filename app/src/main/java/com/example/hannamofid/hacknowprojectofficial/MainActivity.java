83026b00596fcc522032d5e179eb4d1f0b7d6fe8
Hanna Mofid <hmofid03@gmail.com>
2020-04-25 14:14:36 -0700

    Committed the joke button changing colors when you tap on it and when click on feelings button, it take you to a new page.

--- app/src/main/java/com/example/hannamofid/hacknowprojectofficial/MainActivity.java
+++ app/src/main/java/com/example/hannamofid/hacknowprojectofficial/MainActivity.java
@@ -1,13 +1,86 @@
 package com.example.hannamofid.hacknowprojectofficial;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    android.widget.Button feelingsButton;
+    android.widget.Button jokeButton;
+   android.widget.Button songButton;
+   android.widget.RelativeLayout background;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        feelingsButton = (android.widget.Button) findViewById(R.id.button1);
+        jokeButton = (android.widget.Button) findViewById(R.id.button2);
+        songButton = (android.widget.Button) findViewById(R.id.button3);
+
+        setUpListeners();
     }
-}
+
+
+        public void changeColors(){
+                   int redColor;
+                   int greenColor;
+                   int blueColor;
+
+                   redColor = (int) (Math.random() *256);
+                   greenColor = (int) (Math.random() *256);
+                   blueColor = (int) (Math.random() *256);
+
+                   jokeButton.setBackgroundColor(Color.rgb(redColor, greenColor, blueColor));
+
+        }
+
+        public void openActivity2(){
+
+        Intent i = new Intent(this, Activity2.class);
+        startActivity(i);
+
+
+        }
+
+
+
+
+        private void setUpListeners(){
+
+            jokeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //where action happens!!!
+                    changeColors();
+
+
+                }
+            });
+
+            feelingsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //where action happens!!!
+
+                    openActivity2();
+                }
+            });
+
+
+
+
+
+
+
+        }
+
+
+    }
+
